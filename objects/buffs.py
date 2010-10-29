9d35437a18fdda3bf08b958229f68cc6aa9f02c8
Raconzor <pedben@gmail.com>
2010-10-28 23:44:16 -0400

    Adding values for most buffs.  Todo - way to retrieve values

--- objects/buffs.py
+++ objects/buffs.py
@@ -3,18 +3,18 @@ class Buffs(object):
     # support other classes with this framework.
 
     allowed_buffs = frozenset([
-        'short_term_haste_buff',            # Heroism/Blood Lust, Time Warp
-        'stat_multiplier_buff',             # Mark of the Wild, Blessing of Kings
-        'crit_chance_buff',                 # Leader of the Pack, HAT, Elemental Oath, Rampage
-        'all_damage_buff',                  # Arcane Tactics, Communion, Ferocious Inspiration
-        'melee_haste_buff',                 # Windfury, Improved Icy Talons
-        'attack_power_buff',                # Trueshot, Unleashed Rage, Abomination's Might, Blessing of Might
-        'str_and_agi_buff',                 # Horn of Winter, Strength of Earth, Battle Shout
-        'armor_debuff',                     # Sunder, Expose Armor, Faerie Fire
-        'physical_vulnerability_debuff',    # Brittle Bones, Savage Combat, Blood Frenzy
-        'spell_damage_debuff',              # Ebon Plaguebringer, Master Poisoner, Earth and Moon, Curse of Elements
-        'spell_crit_debuff',                # Critical Mass, Shadow and Flame
-        'bleed_damage_debuff',              # Hemo, Mangle, Trauma
+        'short_term_haste_buff',                # Heroism/Blood Lust, Time Warp
+        'stat_multiplier_buff': 1.05,           # Mark of the Wild, Blessing of Kings
+        'crit_chance_buff': 1.05,               # Leader of the Pack, HAT, Elemental Oath, Rampage
+        'all_damage_buff': 1.03,                # Arcane Tactics, Communion, Ferocious Inspiration
+        'melee_haste_buff': 1.1,                # Windfury, Improved Icy Talons
+        'attack_power_buff': 1.1,               # Trueshot, Unleashed Rage, Abomination's Might, Blessing of Might
+        'str_and_agi_buff': 1395,               # Horn of Winter, Strength of Earth, Battle Shout
+        'armor_debuff': 0.88,                   # Sunder, Expose Armor, Faerie Fire
+        'physical_vulnerability_debuff': 1.04,  # Brittle Bones, Savage Combat, Blood Frenzy
+        'spell_damage_debuff': 1.08,            # Ebon Plaguebringer, Master Poisoner, Earth and Moon, Curse of Elements
+        'spell_crit_debuff': 1.05,              # Critical Mass, Shadow and Flame
+        'bleed_damage_debuff': 1.3,             # Hemo, Mangle, Trauma
     ])
     
     def __init__(self, *args):
