ae8a865119e54796b685fc207d88a61a4f253ed4
dazer <nextorniquete@gmail.com>
2010-11-01 00:59:19 -0700

    Added a unique function to handle talent modifiers. Included formulas for SS, hemo, ambush, RvS, VW, Rup, Evis and Env

--- calcs/rogue/__init__.py
+++ calcs/rogue/__init__.py
@@ -1,11 +1,11 @@
 from calcs import DamageCalculator
 
 class RogueDamageCalculator(DamageCalculator):
-    # Functions of general use to rogue damage calculation go here.  If a
+    # Functions of general use to rogue damage calculation go here. If a
     # calculation will reasonably used for multiple classes, it should go in
-    # calcs.DamageCalculator instead.  If its a specific intermediate
+    # calcs.DamageCalculator instead. If its a specific intermediate
     # value useful to only your calculations, when you extend this you should
-    # put the calculations in your object.  But there are things - like
+    # put the calculations in your object. But there are things - like
     # backstab damage as a function of AP - that (almost) any rogue damage
     # calculator will need to know, so things like that go here.
 
@@ -19,32 +19,55 @@ class RogueDamageCalculator(DamageCalculator):
         else:
             return .5
 
-    def assassins_resolve(self):
-        return self.talents.is_assassination_rogue() and (self.stats.mh.type == 'dagger')
+    def talents_modifiers(opportunity=False, coup_de_grace=False,
+                          executioner=False, aggression=False,
+                          improved_sinister_strike=False,
+                          improved_ambush=False, potent_poisons=False,
+                          assassins_resolve=True):
+        # This funcion gets called in every ability affected by talents
+        # Parameters are boleans distinguisihing which talents affect the
+        # spell in question. It returns the final modifier for their
+        # respective additive/multiplicative values
+        base_modifier = 1
+        if opportunity:
+            base_modifier += .1 * (self.talents.subtlety.opportunity)
+        if coup_de_grace:
+            cdg_tuple = (0, .07, .14, .2)
+            base_modifier += cdg_tuple[self.talents.asssassination.coup_de_grace]
+        if executioner and self.talents.is_subtlety_rogue():
+            base_modifier += .2 + .025 * self.stats.get_mastery_from_rating()
+        if aggression:
+            base_modifier += .05 * (self.talents.combat.aggression)
+        if improved_sinister_strike:
+            base_modifier += .1 * (self.talents.combat.improved_sinister_strike)
+        if improved_ambush:
+            base_modifier += .05 * (self.talents.subtlety.improved_ambush)
+        # Multiplicative modifiers may need to be handled in a more elegant way
+        if assassins_resolve and self.talents.is_assassination_rogue() and (self.stats.mh.type == 'dagger'):
+            base_modifier *= 1.15
+        if potent_poisons and self.talents.is_assassination_rogue():
+            base_modifier *= 1.2 + .025 * self.stats.get_mastery_from_rating()
 
+        return base_modifier
 
     def backstab_damage(self, ap):
         weapon_damage = self.stats.mh.normalized_damage(ap)
+        multiplier = talents_modifiers(opportunity=True, aggression=True)
+        percentage_damage_bonus = 2
+        if self.talents.is_subtlety_rogue():
+            percentage_damage_bonus += .25
 
-        opportunity_multiplier = 1 + .1 * (self.talents.subtlety.opportunity)
-        aggression_multiplier = 1 + .05 * (self.talents.combat.aggression)
-        damage = 2 * (weapon_damage + 345) * opportunity_multiplier * aggression_multiplier
-
-        if self.assassins_resolve():
-            damage *= 1.15
+        damage = percentage_damage_bonus * (weapon_damage + 345) * multiplier
 
         return damage
 
     def mutilate_damage(self, ap, is_poisoned=True):
         mh_weapon_damage = self.stats.mh.normalized_damage(ap)
         oh_weapon_damage = self.stats.oh.normalized_damage(ap)
+        multiplier = talents_modifiers(opportunity=True)
 
-        opportunity_multiplier = 1 + .1 * (self.talents.subtlety.opportunity)
-
-        # Including Assassin's Resolve without checking, as it's impossible to
-        # mutilate without Assassin's Resolve being active.
-        mh_damage = (1.5 * mh_weapon_damage + 201) * opportunity_multiplier * 1.15
-        oh_damage = self.oh_penalty() * (1.5 * oh_weapon_damage + 201) * opportunity_multiplier * 1.15
+        mh_damage = 1.5 * (mh_weapon_damage + 201) * multiplier
+        oh_damage = 1.5 * (self.oh_penalty() * oh_weapon_damage + 201) * multiplier
 
         if is_poisoned:
             mh_damage *= 1.2
@@ -52,10 +75,90 @@ class RogueDamageCalculator(DamageCalculator):
 
         return mh_damage, oh_damage
 
+    def sinister_strike_damage(self, ap):
+        weapon_damage = self.stats.mh.normalized_damage(ap)
+        multiplier = talents_modifiers(aggression=True, improved_sinister_strike=True)
+
+        damage = (weapon_damage + 200) * multiplier
+
+        return damage
+
+    def hemorrhage_damage(self):
+        weapon_damage = self.stats.mh.normalized_damage(ap)
+
+        if self.stats.mh.type == 'dagger':
+            percentage_damage_bonus = 1.595
+        else:
+            percentage_damage_bonus = 1.1
+        if self.talents.is_subtlety_rogue():
+            percentage_damage_bonus += .25
+
+        damage = percentage_damage_bonus * weapon_damage
+
+        return damage
+
+    def ambush_damage(self, ap):
+        weapon_damage = self.stats.mh.normalized_damage(ap)
+        multiplier = talents_modifiers(opportunity=True, improved_ambush=True)
+
+        if self.stats.mh.type == 'dagger':
+            damage = 2.7493 * weapon_damage + 367 * 2.75
+        else:
+            damage = 1.9 * weapon_damage + 367 * 1.9
+
+        return damage
+
+    def revealing_strike_damage(self, ap):
+        weapon_damage = self.stats.mh.normalized_damage(ap)
+
+        damage = 1.25 * weapon_damage
+
+        return damage
+
+    def venomous_wounds_damage(self, ap):
+        damage = 363 + .135 * ap
+        return damage
+
+    def rupture_damage(self, ap, cp):
+        # Assassasin's resolve was tested on melee, poisons, weapon strikes and
+        # ap strikes, not bleeds. Although there's no reason to believe it doesn't
+        # affect bleeds, I'm setting it to false until some testing is done
+        multiplier = talents_modifiers(executioner=True, assassins_resolve=False)
+
+        duration = (6 + cp * 2)
+        if self.glyphs.rupture():
+            duration +=4
+
+        ap_multiplier_tuple = (0, .015, .024, .03, .03428571, .0375)
+        tick_damage = (142 + 20 * cp + ap_multiplier_tuple[cp] * ap) * multiplier
+        damage = tick_damage * .5 * duration
+
+        return tick_damage, duration, damage
+
+    def eviscerate_damage(self, ap, cp):
+        multiplier = talents_modifiers(coup_de_grace=True, aggression=True, executioner=True)
+
+        ap_multiplier_tuple = (0, .091, .182, .273, .364, .455)
+        low_end_damage = (183 + 536 * cp + ap_multiplier_tuple[cp] * ap) * multiplier
+        high_end_damage = (549 + 536 * cp + ap_multiplier_tuple[cp] * ap) * multiplier
+        average_damage = ((183 + 549) / 2 + 536 * cp + ap_multiplier_tuple[cp] * ap) * multiplier
+
+        return low_end_damage, high_end_damage, average_damage
+
+    def envenom_damage(self, ap, cp):
+        # Envemom has a dependency on dp_charges too; but being unlikely to be used out of builds
+        # with master poisoner I'm not including that for the moment
+        multiplier = talents_modifiers(coup_de_grace=True, executioner=True,
+                                       assassins_resolve=False potent_poisons=True)
+
+        damage = (241 * cp + .09 * cp * ap) * multiplier
+
+        return damage
+
     def melee_crit_rate(self, agi=None, crit=None):
         if agi == None:
             agi = self.stats.agi
-        base_crit = self.AGI_CRIT_INTERCEPT + agi /  self.AGI_PER_CRIT
+        base_crit = self.AGI_CRIT_INTERCEPT + agi / self.AGI_PER_CRIT
         base_crit += self.stats.get_crit_from_rating(crit)
         return base_crit + self.buffs.buff_all_crit()
 
