f71720453fbb0e03b97c7c573a7d2d8b2495382d
dazer <nextorniquete@gmail.com>
2010-11-01 10:45:49 -0700

    Fixing bugs. Potent poisons now stacks additively. Added formulas for WP IP DP and garrote

--- calcs/rogue/__init__.py
+++ calcs/rogue/__init__.py
@@ -21,9 +21,9 @@ class RogueDamageCalculator(DamageCalculator):
 
     def talents_modifiers(opportunity=False, coup_de_grace=False,
                           executioner=False, aggression=False,
-                          improved_sinister_strike=False,
+                          improved_sinister_strike=False, vile_poisons=False
                           improved_ambush=False, potent_poisons=False,
-                          assassins_resolve=True):
+                          assassins_resolve=True, mastery=None):
         # This funcion gets called in every ability affected by talents
         # Parameters are boleans distinguisihing which talents affect the
         # spell in question. It returns the final modifier for their
@@ -35,18 +35,21 @@ class RogueDamageCalculator(DamageCalculator):
             cdg_tuple = (0, .07, .14, .2)
             base_modifier += cdg_tuple[self.talents.asssassination.coup_de_grace]
         if executioner and self.talents.is_subtlety_rogue():
-            base_modifier += .2 + .025 * self.stats.get_mastery_from_rating()
+            base_modifier += .025 * self.stats.get_mastery_from_rating(mastery)
         if aggression:
             base_modifier += .05 * (self.talents.combat.aggression)
         if improved_sinister_strike:
             base_modifier += .1 * (self.talents.combat.improved_sinister_strike)
+        if vile_poisons:
+            vp_tuple = (0, .07, .14, .2)
+            base_modifier += vp_tuple[self.talents.asssassination.vile_poisons]
         if improved_ambush:
             base_modifier += .05 * (self.talents.subtlety.improved_ambush)
+        if potent_poisons and self.talents.is_assassination_rogue():
+            base_modifier += .025 * self.stats.get_mastery_from_rating(mastery)
         # Multiplicative modifiers may need to be handled in a more elegant way
         if assassins_resolve and self.talents.is_assassination_rogue() and (self.stats.mh.type == 'dagger'):
             base_modifier *= 1.15
-        if potent_poisons and self.talents.is_assassination_rogue():
-            base_modifier *= 1.2 + .025 * self.stats.get_mastery_from_rating()
 
         return base_modifier
 
@@ -92,6 +95,10 @@ class RogueDamageCalculator(DamageCalculator):
             percentage_damage_bonus = 1.1
         if self.talents.is_subtlety_rogue():
             percentage_damage_bonus += .25
+            # This should probably be tested at some point to make sure there isn't
+            # some weird interaction with the dagger-or-not logic - that is,
+            # its possible that the computation the do internally would be
+            # (1.1 + .25) * 1.45 rather than 1.1 * 1.45 + .25.
 
         damage = percentage_damage_bonus * weapon_damage
 
@@ -102,21 +109,62 @@ class RogueDamageCalculator(DamageCalculator):
         multiplier = talents_modifiers(opportunity=True, improved_ambush=True)
 
         if self.stats.mh.type == 'dagger':
-            damage = 2.7493 * weapon_damage + 367 * 2.75
+            damage = (2.7493 * weapon_damage + 367 * 2.75) * multiplier
         else:
-            damage = 1.9 * weapon_damage + 367 * 1.9
+            damage = (1.9 * weapon_damage + 367 * 1.9) * multiplier
 
         return damage
 
     def revealing_strike_damage(self, ap):
-        weapon_damage = self.stats.mh.normalized_damage(ap)
+        weapon_damage = self.stats.mh.damage(ap)
 
         damage = 1.25 * weapon_damage
 
         return damage
 
-    def venomous_wounds_damage(self, ap):
-        damage = 363 + .135 * ap
+    def venomous_wounds_damage(self, ap, mastery):
+        multiplier = talents_modifiers(potent_poisons=True, assassins_resolve=False, mastery)
+
+        damage = (363 + .135 * ap) * multiplier
+        # need values for lvl 85
+
+        return damage
+
+    def instant_poison_damage(self, ap, mastery):
+        multiplier = talents_modifiers(potent_poisons=True, vile_poisons = True,
+                                       assassins_resolve=False, mastery)
+
+        low_end_damage = (300 + 0.09 * ap) * multiplier
+        high_end_damage = (400 + 0.09 * ap) * multiplier
+        average_damage = ((300 + 400) / 2 + 0.09 * ap) * multiplier
+        # pulled from the assassination spreadsheet; need values for lvl 85
+
+        return low_end_damage, high_end_damage, average_damage
+
+    def deadly_poison_damage(self, ap, mastery):
+        # also dependancy on dp_stacks=5 but leaving it for now
+        multiplier = talents_modifiers(potent_poisons=True, vile_poisons = True,
+                                       assassins_resolve=False, mastery)
+
+        damage = (296 + ,108 * ap) * multiplier
+        # pulled from the assassination spreadsheet; need values for lvl 85
+
+        return damage
+
+    def wound_poison_damage(self, ap, mastery):
+        multiplier = talents_modifiers(potent_poisons=True, vile_poisons = True,
+                                       assassins_resolve=False, mastery)
+
+        damage = (231 + ,036 * ap) * multiplier
+        # pulled from the combat spreadsheet; need values for lvl 85
+
+        return damage
+
+    def garrote_damage(self, ap):
+        multiplier = talents_modifiers(opportunity=True, assassins_resolve=False)
+
+        damage = (133 +  ap * 1 * 0.07) * 6 * multiplier
+
         return damage
 
     def rupture_damage(self, ap, cp):
@@ -148,8 +196,7 @@ class RogueDamageCalculator(DamageCalculator):
     def envenom_damage(self, ap, cp):
         # Envemom has a dependency on dp_charges too; but being unlikely to be used out of builds
         # with master poisoner I'm not including that for the moment
-        multiplier = talents_modifiers(coup_de_grace=True, executioner=True,
-                                       assassins_resolve=False potent_poisons=True)
+        multiplier = talents_modifiers(coup_de_grace=True, executioner=True, assassins_resolve=False)
 
         damage = (241 * cp + .09 * cp * ap) * multiplier
 
