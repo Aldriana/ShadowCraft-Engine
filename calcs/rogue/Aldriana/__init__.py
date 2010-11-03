89991d7fceb2beb5e37e9790f5e345825ac15bbf
Aldriana <aldriana@elitistjerks.com>
2010-11-03 00:46:15 -0700

    More damage calculations.

--- calcs/rogue/Aldriana/__init__.py
+++ calcs/rogue/Aldriana/__init__.py
@@ -63,6 +63,10 @@ class AldrianasRogueDamageCalculator(RogueDamageCalculator):
         self.base_agility *= self.buffs.stat_multiplier()
         self.base_melee_crit_rate = self.melee_crit_rate(self.base_agility)
 
+        self.relentless_strikes_energy_return_per_cp = [0, 1.75, 3.5, 5][self.talents.subtlety.relentless_strikes]
+
+        self.attack_speed_multiplier = 1.4 * self.buffs.melee_haste_multiplier() * self.stats.get_haste_multiplier_from_rating()
+
     def assassination_dps_breakdown_mutilate(self):
         mutilate_energy_cost = 48 + 12 / self.one_hand_melee_hit_chance()
         if self.glyphs.mutilate:
@@ -74,7 +78,63 @@ class AldrianasRogueDamageCalculator(RogueDamageCalculator):
 
         seal_fate_proc_rate = 1 - (1 - mutilate_base_crit_rate * .5 * self.talents.assassination.seal_fate) ** 2
         cp_per_mut = {2: 1 - seal_fate_proc_rate, 3: seal_fate_proc_rate}
-        self.get_cp_distribution_for_cycle(cp_per_mut, self.settings.cycle.min_envenom_size_mutilate)
+        cp_distribution = self.get_cp_distribution_for_cycle(cp_per_mut, self.settings.cycle.min_envenom_size_mutilate)
+
+        # This cycle need a *lot* of work, but in the interest of getting some
+        # sort of numbers out of this, I'm going to go with ye olde cheap hack
+        # for the moment.
+
+        muts_per_finisher = 0
+        cp_per_finisher = 0
+        for (cps, muts), probability in cp_distribution.items():
+            muts_per_finisher += muts * probability
+            cp_per_finisher += cps * probability
+
+        energy_for_rupture = muts_per_finisher * mutilate_energy_cost + self.rupture_energy_cost - cp_per_finisher * self.relentless_strikes_energy_return_per_cp
+        rupture_downtime = .5 * energy_for_rupture / self.baseline_energy_regen
+        average_rupture_length = 2 * (3 + cp_per_finisher + 2 * self.glyphs.rupture)
+        average_cycle_length = rupture_downtime + average_rupture_length
+
+        energy_for_envenoms = average_rupture_length * self.energy_regen_rupture_up - .5 * energy_for_rupture
+        envenom_energy_cost = muts_per_finisher * mutilate_energy_cost + self.envenom_energy_cost - cp_per_finisher * self.relentless_strikes_energy_return_per_cp
+        envenoms_per_cycle = energy_for_envenoms / envenom_energy_cost
+
+        envenoms_per_second = envenoms_per_cycle / average_cycle_length
+        ruptures_per_second = 1 / average_cycle_length
+        mutilates_per_second = (envenoms_per_second + ruptures_per_second) * muts_per_finisher
+
+        ticks_per_rupture = average_rupture_length / 2
+        venomous_wounds_per_second = ticks_per_rupture * ruptures_per_second * .3 * self.talents.assassination.venomous_wounds
+
+        mh_autoattacks_per_second = self.attack_speed_multiplier / self.stats.mh.speed
+        oh_autoattacks_per_second = self.attack_speed_multiplier / self.stats.oh.speed
+
+        total_mh_hits_per_second = mh_autoattacks_per_second + mutilates_per_second + envenoms_per_second + ruptures_per_second
+        total_oh_hits_per_second = oh_autoattacks_per_second + mutilates_per_second
+
+        if self.settings.mh_poison == 'ip':
+            ip_base_proc_rate = .3 * self.stats.mh.speed / 1.4
+        else:
+            ip_base_proc_rate = .3 * self.stats.oh.speed / 1.4
+
+        ip_envenom_proc_rate = ip_base_proc_rate * 1.5
+
+        dp_base_proc_rate = .5
+        dp_envenom_proc_rate = dp_base_proc_rate * 1.5
+
+        envenom_uptime = min(envenoms_per_second * (cp_per_finisher + 1), 1)
+        avg_ip_proc_rate = ip_base_proc_rate * (1 - envenom_uptime) + ip_envenom_proc_rate * envenom_uptime
+        avg_dp_proc_rate = dp_base_proc_rate * (1 - envenom_uptime) + dp_envenom_proc_rate * envenom_uptime
+
+        if self.settings.mh_poison == 'ip':
+            mh_poison_procs = avg_ip_proc_rate * total_mh_hits_per_second
+            oh_poison_procs = avg_dp_proc_rate * total_oh_hits_per_second
+        else:
+            mh_poison_procs = avg_dp_proc_rate * total_mh_hits_per_second
+            oh_poison_procs = avg_ip_proc_rate * total_oh_hits_per_second
+
+        ip_per_second = mh_poison_procs + oh_poison_procs
+        dp_per_second = 1./3
 
     def assassination_dps_breakdown_backstab(self):
         backstab_base_crit_rate = self.base_melee_crit_rate + self.stats.gear_buffs.rogue_t11_2pc_crit_bonus() + .1 * self.talents.assassination.puncturing_wounds
