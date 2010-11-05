8e228dd2592cc39380aabc9be6efba1e651d5961
julienp <poissonnier@gmail.com>
2010-11-05 16:31:01 +0100

    TestRogueGlyphs

--- tests/tests.py
+++ tests/tests.py
@@ -4,6 +4,7 @@ sys.path.append(".")
 
 from objects_tests.buffs_tests import TestBuffsTrue, TestBuffsFalse
 from objects_tests.stats_tests import TestStats
+from objects_tests.rogue_tests.rogue_glyphs_tests import TestRogueGlyphs
 
 if __name__ == "__main__":
     unittest.main()
