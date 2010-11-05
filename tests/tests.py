ef0ec63c756fc89132e3c97d3403b034ac753c9b
julienp <poissonnier@gmail.com>
2010-11-05 16:32:40 +0100

    DamageCalculatorTest

--- tests/tests.py
+++ tests/tests.py
@@ -2,6 +2,7 @@ import unittest
 import sys
 sys.path.append(".")
 
+from calcs_tests import DamageCalculatorTest
 from objects_tests.buffs_tests import TestBuffsTrue, TestBuffsFalse
 from objects_tests.stats_tests import TestStats
 from objects_tests.rogue_tests.rogue_glyphs_tests import TestRogueGlyphs
