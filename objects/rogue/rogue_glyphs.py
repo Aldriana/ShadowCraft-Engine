61b855de1d1fe5babdcec0a5e7c3eadba47253b0
Aldriana <aldriana@elitistjerks.com>
2010-11-06 11:05:53 -0700

    Adding the rest of the rogue glyphs

--- objects/rogue/rogue_glyphs.py
+++ objects/rogue/rogue_glyphs.py
@@ -5,6 +5,43 @@ class RogueGlyphs(glyphs.Glyphs):
     # for the initial set of calculations.
     
     allowed_glyphs = frozenset([
-        'backstab', 'mutilate', 'rupture', 'slice_and_dice', 'vendetta',
+        # Prime
+        'adrenaline_rush',
+        'backstab',
+        'eviscerate',
+        'hemorrhage',
+        'killing_spree',
+        'mutilate',
+        'revealing_strike',
+        'rupture',
+        'shadow_dance',
+        'sinister_strike',
+        'slice_and_dice',
+        'vendetta',
+        # Major
+        'ambush',
+        'blade_flurry',
+        'blind',
+        'cloak_of_shadows',
+        'crippling_poison',
+        'deadly_throw',
+        'evasion',
+        'expose_armor',
+        'fan_of_knives',
+        'feint',
+        'garrote',
+        'gouge',
+        'kick',
+        'preparation',
+        'sap',
+        'sprint',
         'tricks_of_the_trade',
+        'vanish',
+        # Minor
+        'blurred_speed',
+        'distract',
+        'pick_lock',
+        'pick_pocket',
+        'poisons',
+        'safe_fall'
     ])
