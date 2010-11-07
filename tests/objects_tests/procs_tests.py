344158996f236ed4c1cf335666090299f48a1cfd
julienp <poissonnier@gmail.com>
2010-11-07 16:35:10 +0100

    re-add __init__ test

--- tests/objects_tests/procs_tests.py
+++ tests/objects_tests/procs_tests.py
@@ -5,6 +5,11 @@ class TestProcsList(unittest.TestCase):
     def setUp(self):
         self.procsList = procs.ProcsList('darkmoon_card_hurricane','heroic_left_eye_of_rajh')
     
+    def test__init__(self):
+        self.assertRaises(AssertionError, procs.ProcsList, 'fake_proc')
+        self.procsList = procs.ProcsList('darkmoon_card_hurricane')
+        self.assertEqual(len(self.procsList.get_all_procs_for_stat(stat=None)), 1)
+    
     def test__getattr__(self):
         self.assertRaises(AttributeError, self.procsList.__getattr__, 'fake_proc')
         self.assertTrue(self.procsList.darkmoon_card_hurricane)
