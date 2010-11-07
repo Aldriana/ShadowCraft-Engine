c695e0edb3f5da0fad9ff19251bce99688a35ce8
julienp <poissonnier@gmail.com>
2010-11-07 16:30:01 +0100

    move lookups from __init__ to __getattr_, check for AttributeError

--- tests/objects_tests/procs_tests.py
+++ tests/objects_tests/procs_tests.py
@@ -5,12 +5,10 @@ class TestProcsList(unittest.TestCase):
     def setUp(self):
         self.procsList = procs.ProcsList('darkmoon_card_hurricane','heroic_left_eye_of_rajh')
     
-    def test__init__(self):
+    def test__getattr__(self):
+        self.assertRaises(AttributeError, self.procsList.__getattr__, 'fake_proc')
         self.assertTrue(self.procsList.darkmoon_card_hurricane)
         self.assertFalse(self.procsList.fluid_death)
-    
-    def test__getattr__(self):
-        pass
     
     def test_get_all_procs_for_stat(self):
         self.assertEqual(len(self.procsList.get_all_procs_for_stat(stat=None)), 2)
