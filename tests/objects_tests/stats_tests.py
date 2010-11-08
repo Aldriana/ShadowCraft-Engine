4abbd59befab9920386caa3216f87f3d69152ef0
julienp <poissonnier@gmail.com>
2010-11-08 16:35:06 +0100

    adjust tests for new level handling

--- tests/objects_tests/stats_tests.py
+++ tests/objects_tests/stats_tests.py
@@ -8,35 +8,35 @@ class TestStats(unittest.TestCase):
     def test_stats(self):
         self.assertEqual(self.stats.agi, 3485)
     
+    def test_set_constants_for_level(self):
+        self.assertRaises(AssertionError, self.stats.__setattr__, 'level', 86)
+    
     def test_get_mastery_from_rating(self):
-        self.assertRaises(AssertionError, self.stats.get_mastery_from_rating, 86)
-        self.assertAlmostEqual(self.stats.get_mastery_from_rating(85), 8 + 666/179.279998779296875)
-        self.assertAlmostEqual(self.stats.get_mastery_from_rating(85, 100),  8 + 100/179.279998779296875)
+        self.assertAlmostEqual(self.stats.get_mastery_from_rating(), 8 + 666/179.279998779296875)
+        self.assertAlmostEqual(self.stats.get_mastery_from_rating(100),  8 + 100/179.279998779296875)
+        self.stats.level = 80
+        self.assertAlmostEqual(self.stats.get_mastery_from_rating(), 8 + 666/45.906)
+        self.assertAlmostEqual(self.stats.get_mastery_from_rating(100),  8 + 100/45.906)
     
     def test_get_melee_hit_from_rating(self):
-        self.assertRaises(AssertionError, self.stats.get_melee_hit_from_rating, 86)
-        self.assertAlmostEqual(self.stats.get_melee_hit_from_rating(85), .01 * 1086/120.109001159667969)
-        self.assertAlmostEqual(self.stats.get_melee_hit_from_rating(85, 100), .01 * 100/120.109001159667969)
+        self.assertAlmostEqual(self.stats.get_melee_hit_from_rating(), .01 * 1086/120.109001159667969)
+        self.assertAlmostEqual(self.stats.get_melee_hit_from_rating(100), .01 * 100/120.109001159667969)
     
     def test_get_expertise_from_rating(self):
-        self.assertRaises(AssertionError, self.stats.get_expertise_from_rating, 86)
-        self.assertAlmostEqual(self.stats.get_expertise_from_rating(85), .01 * 641/(30.027200698852539*4))
-        self.assertAlmostEqual(self.stats.get_expertise_from_rating(85, 100), .01 * 100/(30.027200698852539*4))
+        self.assertAlmostEqual(self.stats.get_expertise_from_rating(), .01 * 641/(30.027200698852539*4))
+        self.assertAlmostEqual(self.stats.get_expertise_from_rating(100), .01 * 100/(30.027200698852539*4))
 
     def test_get_spell_hit_from_rating(self):
-        self.assertRaises(AssertionError, self.stats.get_spell_hit_from_rating, 86)
-        self.assertAlmostEqual(self.stats.get_spell_hit_from_rating(85), .01 * 1086/102.445999145507812)
-        self.assertAlmostEqual(self.stats.get_spell_hit_from_rating(85, 100), .01 * 100/102.445999145507812)
+        self.assertAlmostEqual(self.stats.get_spell_hit_from_rating(), .01 * 1086/102.445999145507812)
+        self.assertAlmostEqual(self.stats.get_spell_hit_from_rating(100), .01 * 100/102.445999145507812)
     
     def test_get_crit_from_rating(self):
-        self.assertRaises(AssertionError, self.stats.get_crit_from_rating, 86)
-        self.assertAlmostEqual(self.stats.get_crit_from_rating(85), .01 * 1517/179.279998779296875)
-        self.assertAlmostEqual(self.stats.get_crit_from_rating(85, 100), .01 * 100/179.279998779296875)
+        self.assertAlmostEqual(self.stats.get_crit_from_rating(), .01 * 1517/179.279998779296875)
+        self.assertAlmostEqual(self.stats.get_crit_from_rating(100), .01 * 100/179.279998779296875)
 
     def test_get_haste_multiplier_from_rating(self):
-        self.assertRaises(AssertionError, self.stats.get_haste_multiplier_from_rating, 86)
-        self.assertAlmostEqual(self.stats.get_haste_multiplier_from_rating(85), 1 + .01 * 899/128.057006835937500)
-        self.assertAlmostEqual(self.stats.get_haste_multiplier_from_rating(85, 100), 1 + .01 * 100/128.057006835937500)
+        self.assertAlmostEqual(self.stats.get_haste_multiplier_from_rating(), 1 + .01 * 899/128.057006835937500)
+        self.assertAlmostEqual(self.stats.get_haste_multiplier_from_rating(100), 1 + .01 * 100/128.057006835937500)
 
 class TestWeapon(unittest.TestCase):
     def setUp(self):
