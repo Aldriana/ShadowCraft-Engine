a57c719e6d0aab53f40f0d823265a068be2d3551
dazer <nextorniquete@gmai.com>
2010-11-16 02:55:15 +0100

    Changed the behaviour of set_language. It doesn't default to Spanish but to code strings if no .mo file is found. Included a phony 'en' flavor with a flaged test string.

--- core/i18n.py
+++ core/i18n.py
@@ -5,27 +5,28 @@ import __builtin__
 
 __builtin__._ = gettext.gettext
 
-SUPPORTED_LANGUAGES = ['es_ES', 'es']       # Add here languages as we create .mo files
-TRANSLATION_DOMAIN = 'SCE'                  # Domain: this needs to be the name of our .mo files
+# Domain: this needs to be the name of our .mo files
+TRANSLATION_DOMAIN = 'SCE'
 LOCALE_DIR = os.path.join(os.path.dirname(__file__), "locale")
 
 def set_language(language):
     # This function will install gettext as the _() function and use the
-    # language specified. It won't traslate anything if given a not supported
+    # language specified. It will fall back to code strings if given a not supported
     # language. Note that the 'local' value only makes sense when not running from
     # the hosted online version.
     if language == 'local':
-        # Setting up a list of locales in your machine, append those
-        # that we have, and asign them to the _() function
+        # Setting up a list of locales in your machine and asign them to the _() function
         languages_list = []
+
         default_local_language, encoding = locale.getdefaultlocale()
         if (default_local_language):
             languages_list = [default_local_language]
-        lang = os.environ.get('LANGUAGE', None)
-        if (lang):
-            languages_list += lang.split(":")
-        languages_list += SUPPORTED_LANGUAGES
-        gettext.translation(TRANSLATION_DOMAIN, LOCALE_DIR, languages=languages_list).install(unicode=True)
 
-    elif language in SUPPORTED_LANGUAGES:
+        gnu_lang = os.environ.get('LANGUAGE', None)
+        if (gnu_lang):
+            languages_list += gnu_lang.split(":")
+
+        gettext.translation(TRANSLATION_DOMAIN, LOCALE_DIR, fallback=True, languages=languages_list).install(unicode=True)
+
+    else:
         gettext.translation(TRANSLATION_DOMAIN, LOCALE_DIR, fallback=True, languages=[language]).install(unicode=True)
